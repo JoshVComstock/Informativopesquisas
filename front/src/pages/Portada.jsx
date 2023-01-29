bacec5da46479e7a895f58a97b542cea6368c6f1
JoshVComstock <josedanielveizaga@gmail.com>
2023-01-28 22:31:28 -0400

    Josh

---
+++ front/src/pages/Portada.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Portada = () => {
+  return (
+    <Conteiner>
+      portada
+      <img src="" alt="" />
+    </Conteiner>
+  );
+};
+
+export default Portada;
+
+const Conteiner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
